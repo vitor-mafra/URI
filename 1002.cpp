d9d5ad9c1d03821bdf263f24b785de89d1d1a6c8
Vitor  Mafra <vitor.mafra@hotmail.com>
2019-03-23 16:53:19 -0300

    Added 1002 problem solution

---
+++ 1002.cpp
@@ -0,0 +1,22 @@
+/*
+    Vitor de Oliveira Mafra
+    UFMG, March 2018
+*/
+#define PI 3.14159
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+
+    double raio;
+    cin >> raio;
+
+    double area = PI * (raio * raio);
+
+    cout << fixed << showpoint; // defining the setprecision as values after the integer part of the number
+    cout << "A=" << std::setprecision(4) << area << endl;
+
+    return 0;
+}
