73ff34e72f004a195448b17983e6fbe5c5edee5d
Vitor  Mafra <vitor.mafra@hotmail.com>
2019-03-23 16:54:21 -0300

    Add files via upload

---
+++ 1003.cpp
@@ -0,0 +1,18 @@
+/*
+    Vitor de Oliveira Mafra
+    UFMG, March 2018
+*/
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	int A, B;
+
+	cin >> A >> B;
+
+	cout << "SOMA = " << (A+B) << endl;
+
+	return 0;
+}
