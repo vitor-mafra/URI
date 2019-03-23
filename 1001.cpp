8ae8ffa3d8c85cb8b262f25641dd679dc336b50f
Vitor  Mafra <vitor.mafra@hotmail.com>
2019-03-23 16:52:35 -0300

    Added 1001 problem solution

---
+++ 1001.cpp
@@ -0,0 +1,19 @@
+/*
+    Vitor de Oliveira Mafra
+    UFMG, March 2018
+*/
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int a, b;
+
+    cin >> a;
+    cin >> b;
+
+    cout << "X = " << a + b << endl;
+
+    return 0;
+}
