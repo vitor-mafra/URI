7e98ccc33973d250d6c51dd1886e2a8a16d0ef17
Vitor  Mafra <vitor.mafra@hotmail.com>
2019-04-02 20:20:13 -0300

    Add 1007 problem sotution

---
+++ 1007.cpp
@@ -0,0 +1,20 @@
+/*
+    Vitor de Oliveira Mafra
+    UFMG, April 2018
+*/
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	int A, B, C, D;
+	int diferenca;
+
+	cin >> A >> B >> C >> D;
+
+	diferenca = ((A * B) - (C * D));
+	cout << "DIFERENCA = " << diferenca << endl;
+
+	return 0;
+}
