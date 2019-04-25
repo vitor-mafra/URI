d075a199ed4e2613205f25dfa138c1b98afe2129
Vitor  Mafra <vitor.mafra@hotmail.com>
2019-04-25 19:38:36 -0300

    Added 1011 problem solution

---
+++ 1011.cpp
@@ -0,0 +1,23 @@
+/*
+    Vitor de Oliveira Mafra
+    UFMG, April 2018
+*/
+#include <iostream>
+#include <iomanip>
+#define PI 3.14159
+
+using namespace std;
+
+int main(){
+
+    double raio, volume;
+
+    cin >> raio;
+
+    volume = (4.0/3) * PI * (raio*raio*raio); // volume = 4/3 * pi * r^3 
+
+    cout << fixed << showpoint;
+    cout << "VOLUME = " << setprecision(3) << volume << endl;
+
+    return 0;
+}
