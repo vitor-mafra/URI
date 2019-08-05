f5235a737948645772bf964e8d1a938c810b4b18
Vitor  Mafra <vitor.mafra@hotmail.com>
2019-08-05 12:50:47 -0300

    Added 1014 problem solution

---
+++ 1015.cpp
@@ -0,0 +1,23 @@
+/*
+    Vitor de Oliveira Mafra
+    UFMG, April 2018
+*/
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    double x1, y1, x2, y2, distancia;
+
+    cin >> x1 >> y1;
+    cin >> x2 >> y2;
+
+    distancia = sqrt(((x2 - x1)*(x2 - x1)) + ((y2-y1)*(y2-y1)));
+
+    cout << fixed << showpoint;
+    cout << setprecision(4) << distancia << endl;
+
+    return 0;
+}
