a97908ae0777511804d6ac0a297375e8dbe85ac8
Vitor  Mafra <vitor.mafra@hotmail.com>
2019-08-05 18:55:19 -0300

    Added 1017 problem solution

--- iniciante(begginer)/1017.cpp
+++ iniciante(begginer)/1017.cpp
@@ -1,1 +1,27 @@
+/*
+    Vitor de Oliveira Mafra
+    UFMG, August 2019
+*/
+#include <iostream>
+#include <iomanip>
 
+using namespace std;
+
+int main(){
+    double autonomia = 12; // (12km/L)
+    double tempo_de_viagem, velocidade_media, distancia_percorrida, qtd_litros;
+
+    cin >> tempo_de_viagem;
+    cin >> velocidade_media;
+
+    //velocidade = distancia / tempo
+    //distancia = velocidade * tempo
+
+    distancia_percorrida = velocidade_media * tempo_de_viagem;
+    qtd_litros = distancia_percorrida / autonomia; 
+
+    cout << fixed << showpoint;
+    cout << setprecision(3) << qtd_litros << endl;
+
+    return 0;
+}       
